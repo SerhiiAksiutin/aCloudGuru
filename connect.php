adb31a6952f8a89c596afee6263f13751fd7cb4f
SerhiiAksiutin <serhiiaksiutin@icloud.com>
2019-11-07 23:51:39 -0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,5 +7,5 @@ $dbname = "";
 //connection to the database
 $dbhandle = mysql_connect($hostname, $username, $password) or die("Unable to connect to MySQL");
 echo "Connected to MySQL using username - $username, password - $password, host - $hostname<br>";
-$selected = mysql_select_db("$dbname" $dbhandle) or die("Unable to connect to MySQL DB - check the database name and try again.");
+$selected = mysql_select_db("$dbname", $dbhandle) or die("Unable to connect to MySQL DB - check the database name and try again.");
 ?>
